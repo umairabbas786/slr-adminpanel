25c51bba0453e0bb7574c8e52c5ac512ff60e8a0
Umair Abbas <umairabbass786@gmail.com>
2023-06-20 20:43:58 +0500

    php connection added

--- includes/db.php
+++ includes/db.php
@@ -1,26 +1,29 @@
 <?php 
+    session_start();
+    ob_start();
+
     $type = "local";
 
     if($type == "local") {
 
-        $servername = "localhost";
+        $server = "localhost";
         $username = "root";
         $password = "";
         $dbname = "slr";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($server, $username, $password, $dbname);
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
     }
     if($type == "live"){
-        $servername = "localhost";
+        $server = "localhost";
         $username = "umairabbas786_umair";
         $password = "Devils@dvocate007";
         $dbname = "umairabbas786_slr";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($server, $username, $password, $dbname);
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<?php
+include "./includes/db.php";
+$error = "";
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password  = $_POST['password'];
+    echo $email . ' ' . $password;
+
+    $sql = "select * from admins where email = '$email' and password = '$password'";
+    $r = $conn->query($sql);
+    if (mysqli_num_rows($r) >= 1) {
+        $row = mysqli_fetch_assoc($r);
+        $_SESSION['admin'] = $row['id'];
+        header("location: index.php");
+    } else {
+        $error = "Please enter valid credentials";
+    }
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 
@@ -11,7 +33,7 @@
     <meta name="author" content="pixelcave">
     <meta name="robots" content="noindex, nofollow">
 
-    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+    <link rel="shortcut icon" href="/assets/media/logo.png">
     <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
     <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&display=swap">
@@ -24,24 +46,29 @@
             <div class="bg-gd-dusk">
                 <div class="hero-static content content-full bg-body-extra-light">
                     <div class="py-4 px-1 text-center mb-4">
-                        <img src="/assets/media/logo.png" width="200" class="img img-responsive"/>
+                        <img src="/assets/media/logo.png" width="200" class="img img-responsive" />
                         <h1 class="h3 fw-bold mt-5 mb-2">Welcome to Admin Panel</h1>
                         <h2 class="h5 fw-medium text-muted mb-0">Please sign in</h2>
                     </div>
+                    <?php if ($error != '') { ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $error;?>
+                        </div>
+                    <?php } ?>
                     <div class="row justify-content-center px-1">
                         <div class="col-sm-8 col-md-6 col-xl-4">
                             <form class="js-validation-signin" action="index.php" method="POST">
                                 <div class="form-floating mb-4">
-                                    <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Enter your username">
-                                    <label class="form-label" for="login-username">Username</label>
+                                    <input type="email" class="form-control" id="login-username" name="email" placeholder="Enter your username">
+                                    <label class="form-label" for="login-username">Email</label>
                                 </div>
                                 <div class="form-floating mb-4">
-                                    <input type="password" class="form-control" id="login-password" name="login-password" placeholder="Enter your password">
+                                    <input type="password" class="form-control" id="login-password" name="password" placeholder="Enter your password">
                                     <label class="form-label" for="login-password">Password</label>
                                 </div>
                                 <div class="row g-sm mb-4">
                                     <div class="col-12 mb-2">
-                                        <button type="submit" class="btn btn-lg btn-alt-primary w-100 py-3 fw-semibold">
+                                        <button type="submit" class="btn btn-lg btn-alt-primary w-100 py-3 fw-semibold" name="login">
                                             Sign In
                                         </button>
                                     </div>
