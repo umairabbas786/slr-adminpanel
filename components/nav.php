d0ffd33cbb2c9aeb95c3bd72525aa34f490f464e
Umair Abbas <umairabbass786@gmail.com>
2023-06-20 21:01:57 +0500

    nav file updated

--- components/nav.php
+++ components/nav.php
@@ -9,13 +9,13 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn btn-sm btn-alt-secondary" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-user d-sm-none"></i>
-                    <span class="d-none d-sm-inline-block fw-semibold">Admin</span>
+                    <span class="d-none d-sm-inline-block fw-semibold"><?php echo $admin['name'];?></span>
                     <i class="fa fa-angle-down opacity-50 ms-1"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0" aria-labelledby="page-header-user-dropdown">
                     <div class="px-2 py-3 bg-body-light rounded-top">
                         <h5 class="h6 text-center mb-0">
-                            Admin
+                            <?php echo $admin['name'];?>
                         </h5>
                     </div>
                     <div class="p-2">
